75b48f6e0844ef721373352966addeae0971034b
Maxime Lo Re <solfen@hotmail.com>
2016-06-13 14:51:12 +0200

    Nyan Speed

--- Assets/Scripts/NyanCat.cs
+++ Assets/Scripts/NyanCat.cs
@@ -14,10 +14,15 @@ public class NyanCat : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        
+        StartCoroutine(UpdateSpeed());
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        anim.speed = RessourcesManager.intance.distance / Time.time * speedMultiplier;
+	IEnumerator UpdateSpeed () {
+        while (true) {
+            yield return new WaitForSeconds(1f);
+            anim.speed = (RessourcesManager.intance.distance / Time.time * speedMultiplier);
+        }
 	}
 }
