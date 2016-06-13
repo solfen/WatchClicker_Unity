ae2d851dd081e4b99f5feace20eb99de6791ddb0
Maxime Lo Re <solfen@hotmail.com>
2016-06-13 12:11:40 +0200

    Nyan cat

---
+++ Assets/Scripts/NyanCat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NyanCat : MonoBehaviour {
+
+    private float lastDistance;
+    private float currentSpeed;
+    private float goalPosX;
+    private Animator anim;
+
+    public float maxDist = 10;
+    public float speedMultiplier = 0.001f;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.speed = RessourcesManager.intance.distance / Time.time * speedMultiplier;
+	}
+}
